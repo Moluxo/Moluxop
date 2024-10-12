9d87658d78a60e9fefe067e108159f471ed747b1
Moluxo <moluxo@hotmail.com>
2024-10-12 21:32:24 +0200

    actualizacion migrations

--- migrations/2024-08-10-153614_productos/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE productos (
-  id_producto UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  nombre_producto VARCHAR NOT NULL,
-  descripcion TEXT,
-  precio NUMERIC(10,2) NOT NULL,
-  imagen VARCHAR(255)
-);
